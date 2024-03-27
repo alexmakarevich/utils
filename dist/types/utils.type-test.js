3ac3cf6f7051bf9d2d054ea81634489122b8b2ce
alexmakarevich <40922230+alexmakarevich@users.noreply.github.com>
2024-03-27 13:53:23 +0100

    exclude tests

--- dist/types/utils.type-test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var testtypes_1 = require("./testtypes");
-// Enummed
-testtypes_1.assert;
-testtypes_1.assert;
-//# sourceMappingURL=utils.type-test.js.map
